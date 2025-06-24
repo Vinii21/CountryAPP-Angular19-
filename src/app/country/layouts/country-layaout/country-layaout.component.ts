f12341e45ad9e5e22d4eb3a725ab50301ba8c28c
Vinicio Chavarría U <viniciochavarriaugalde@gmail.com>
2025-06-24 07:45:09 -0600

    Componente de menu superior en el modulo de country, componente de daisyUI iconos de ICONIFY

--- src/app/country/layouts/country-layaout/country-layaout.component.ts
+++ src/app/country/layouts/country-layaout/country-layaout.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TopMenuComponent } from "../../components/top-menu/top-menu.component";
 
 @Component({
   selector: 'country-layaout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, TopMenuComponent],
   templateUrl: './country-layaout.component.html',
 })
 export class CountryLayaoutComponent {
